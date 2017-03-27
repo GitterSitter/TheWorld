2e7107a0ed931ab15eadbe1fab96390d5bed3932
Trond <trondtufte89@gmail.com>
2017-03-27 15:02:11 +0200

    update

---
+++ TheWorldCopy/TheWorld/Controllers/AppController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheWorld.Controllers
+{
+    public class AppController : Controller
+    {
+
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+           // throw new InvalidOperationException("Something went really wrong :/");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+
+            if (model.Email.Contains("aol.com"))
+                ModelState.AddModelError("Email", "We dont support aol addresses!");
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From theWorld", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+
+
+    }
+}
---
+++ TheWorldCopy/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,72 @@
+﻿// site.js
+
+
+// Function as expression: This ensures that the variables are not global, and the code within the
+//paranthesis are executed. 
+(function () {
+
+
+    /* Examples with jQuery */
+    //document.getElementById == $
+    //var ele = document.getElementById("username");
+    //ele.innerHTML = "Trond";
+    //main.onmouseenter = function () {
+    //    main.style.backgroundColor = "#888";
+    //};
+      //main.onmouseleave = function () {
+    //    main.style.backgroundColor = "";
+
+    //};
+     //var main = document.getElementById("main");
+
+
+    /*Example with jQuery*/
+
+    //var ele = $("#username");
+    //ele.text = "Trond";
+
+    //var main = $("#main");
+    
+    //main.on("mouseenter", function () {
+    //    main.style.backgroundColor = "#888";
+    //});
+
+    //main.on("mouseleave",  function () {
+    //    main.style.backgroundColor = "";
+
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text() );
+    //});
+
+
+
+
+    var $icon = $("#sidebarToggle i.fa");
+    //Displays the sidebar
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+   
+
+    $("#sidebarToggle").on("click", function () {
+
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+       
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+          
+        } else {
+          
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
+          
+        }
+    });
+
+   
+})();
+// () executes the code of the ananomouse function
+  
